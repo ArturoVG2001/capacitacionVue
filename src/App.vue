2debfbaa50017c32d6e51f3b30837720a94199c8
ArturoVG2001 <jose.villalobos@cceo.com.mx>
2024-03-01 17:34:14 -0600

    Seccion 4: Componentes

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,55 @@
 <script setup>
-import { computed, ref } from 'vue';
-const name = 'vue dinamico';
-let counter = ref(0);
-let numberArray = ref([]);
-let disabledBand = ref(false);
+import BlogPost from './components/BlogPost.vue';
+import { ref, onMounted } from "vue";
+import PagintePost from './components/PaginatePost.vue';
+import LoadingSpinner from './components/LoadingSpinner.vue'
+const posts = ref([]);
+const inicio = ref(0);
+const fin = ref(postXpage);
+const postXpage = 10;
+const loading = ref(true);
 
-const increment = () => (counter.value++);
+const favorito = ref('');
 
-const decrement = () => (counter.value--);
+const cambiarFavorito = (title) => {
+  favorito.value = title;
+};
 
-const restart = () => (counter.value = 0);
 
-const classCounter = () => {
-  if (counter.value === 0) {
-    return 'zero';
-  } else if (counter.value > 0) {
-    return 'positive';
-  } else {
-    return 'negative';
+
+const next = () => {
+  inicio.value += postXpage;
+  fin.value += postXpage;
+}
+
+const prev = () => {
+  inicio.value = inicio.value - postXpage;
+  fin.value = fin.value - postXpage;
+}
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    posts.value = await res.json();
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
   }
 }
 
-const pushNumber = () => {
-  if (numberArray.value.includes(counter.value)) {
-    disabledBand.value = true;
-  } else {
-    numberArray.value.push(counter.value)
-    disabledBand.value = false;
-  }
-}
-
+fetchData();
 </script>
 
 <template>
-  <div class="container text-center mt-3">
-    <h1>Hola {{ name.toUpperCase() }}</h1><br>
-    <h2 :class="classCounter()">{{ counter }}</h2>
-    <div class="btn-group">
-      <button @click="decrement" class="btn btn-success">Disminuir</button>
-      <button @click="restart" class="btn btn-danger">Reiniciar</button>
-      <button @click="increment" class="btn btn-secondary">Aumentar</button>
-      <button @click="pushNumber()" :disabled="disabledBand" class="btn btn-primary">Add</button>
-    </div>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="(num, index) in numberArray" :key="index">{{ num }}</li>
-    </ul>
+  <LoadingSpinner v-if="loading"></LoadingSpinner>
+  <div class="container" v-else>
+    <h1>APP</h1>
+    <h2>Mi post favorito: {{ favorito }}</h2>
+
+    <PagintePost class="mb-2" :next="next" :prev="prev" :inicio="inicio" :fin="fin" :count="posts.length"></PagintePost>
+    <BlogPost v-for="post in posts.slice(inicio, fin)" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
+      :cambiarFavorito="cambiarFavorito" />
   </div>
 </template>
-
-<style>
-.positive {
-  color: green;
-}
-
-.negative {
-  color: red;
-}
-
-.zero {
-  color: peru;
-}
-</style>
---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['prev','next','inicio','fin','count']);
+</script>
+
+<template>
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary" @click="prev" :disabled="inicio <= 0">Preview</button>
+        <button type="button" class="btn btn-primary" @click="next" :disabled="fin >= count">Next</button>
+    </div>
+</template>
